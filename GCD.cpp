8a569867ea410ab1577d61dc0bae0bdd4f1c11ad
Sivaraju35 <sivaraju7032@gmail.com>
2024-05-12 17:23:57 +0530

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int gcd(int a, int b) {
+       while(a>0&&b>0)
+       {
+           if(a>b) a=a%b;
+           else  b=b%a;
+           
+       }
+       if(a==0)
+       return b;
+       else 
+       return a;
+    }
+};
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.gcd(a, b);
+        
+        cout<<res<<endl;
+        
+    }
+}
